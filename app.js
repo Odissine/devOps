e37cda641e34730f49a773603a7c207c7c7a9b98
Odissine <albator74@hotmail.com>
2021-07-13 09:51:53 +0200

    Premier Commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const port = 3001
+
+
+//quand moi serveur web je reçoit une requête de type "GET" d'un client 
+//sur l'url /hello, alors je renvoie "hello world"
+app.get('/coucou', function(request, response) {
+  response.send('<h2>Bonjour tout le monde<h2>')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
