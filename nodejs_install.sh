c4f7b540d233b4465cbffaf70594339c4655cdf9
Odissine <albator74@hotmail.com>
2021-07-13 10:10:27 +0200

    Delete Old SH

--- nodejs_install.sh
+++
@@ -1,7 +0,0 @@
-echo "Debut du script"
-echo "Installation et lancement du serveur Node"
-nodejs -v || sudo apt install nodejs
-npm -v || sudo apt install npm
-test -f ./myapp/app.js && nodejs ./myapp/app.js && echo "Serveur lancé avec succès !"
-test -f ./myapp/app.js && echo "Erreur ... fichier app.js inexistant"
-echo "Fin du script"
