0ffca395865716e811e896c41e17c05e3b8244ec
Odissine <albator74@hotmail.com>
2021-07-13 10:33:53 +0200

    Fichier app modifié

--- app.js
+++ app.js
@@ -6,7 +6,11 @@ const port = 3001
 //quand moi serveur web je reçoit une requête de type "GET" d'un client 
 //sur l'url /hello, alors je renvoie "hello world"
 app.get('/coucou', function(request, response) {
-  response.send('<h2>Bonjour tout le monde<h2>')
+  response.send('<h2>Bonjour tout le monde</h2>')
+})
+
+app.get('/test', function(request, response) {
+  response.send('<h1>Salut les loulous !!!!</h1>')
 })
 
 app.listen(port, () => {
